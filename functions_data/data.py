721695b28ec4f8b8dd5a20a0b27cb85786e1fb4f
DISHANT KUKREJA <dishantkukreja26@gmail.com>
2021-12-19 00:25:02 +0530

    initial commit

---
+++ functions_data/data.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from math import radians, cos, sin, asin, sqrt
+
+
+def read_csv(path: str):
+    return pd.read_csv(path)
+
+def days_table_cover(flight):
+    return flight.groupby(['year'], sort=True).sum()
+
+def departure_cities(flight):
+    count = flight[["origin"]]
+    city =(count.value_counts())
+    return city
+
+def relation_manufacter(flight,planes):
+    relation = flight.merge(planes, how='inner', on=['tailnum', 'year'])
+    most_delay = relation.groupby(['manufacturer'], sort=True).sum().sort_values("dep_delay", ascending=False)
+    return most_delay['dep_delay']
+
+def haversine(lon1, lat1, lon2, lat2):
+
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+
+def main():
+    flight = read_csv('C:/Users//Rev07//Downloads//data//flights.csv')
+    planes = read_csv('C:/Users//Rev07//Downloads//data//planes.csv')
+    airport = read_csv('C:/Users//Rev07//Downloads//data//airports.csv')
+    b = airport.copy()
+
+    print(days_table_cover(flight))
+
+    print("5291016 days in the flights table covers")
+
+    print(departure_cities(flight))
+
+    print("EWR, JFK,LGA are departure cities in the flight database covers")
+
+    print(relation_manufacter(flight, planes).head(1))
+
+    print("BOEING-airplane manufacturer incurred the most delays in the analysis period")
+
+    b['tmp'] = 1
+    b = pd.merge(b, b, on='tmp')
+    b = b[b.CITY_x != b.CITY_y]
+    b['dist'] = b.apply(lambda row: haversine(row['LONGITUDE_x'],
+                                              row['LATITUDE_x'],
+                                              row['LONGITUDE_y'],
+                                              row['LATITUDE_y']), axis=1)
+    b = b[b.dist < 500] #distance between 500 km
+    df1 = b.groupby('CITY_x')['CITY_y'].apply(list)
+    df2 = b.groupby('CITY_x')['CITY_y'].size()
+    connected = (df2.sort_values(ascending=False))
+    print(connected.head(2))
+
+    print("Chicago and New York are the two most connected cities between 500 distance km")
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/test.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import pytest
+from functions_data.data import read_csv,days_table_cover,departure_cities,relation_manufacter
+
+
+def test_read_csv():
+    path='C:/Users//Rev07//PycharmProjects//data2//test//file//flight.csv'
+    df = read_csv(path)
+    assert df.shape == (3,1)
+
+def test_days_table_cover():
+    #Arrange
+    path = 'C:/Users//Rev07//PycharmProjects//data2//test//file//flight.csv'
+    df = pd.read_csv(path, sep='\t')
+    expected = 3
+    # Act
+    result = days_table_cover(df)
+    # Assert
+    assert result.iloc[0][0] == expected
+
+def test_departure_cities():
+    #Arrange
+    path = 'C:/Users//Rev07//PycharmProjects//data2//test//file//flight.csv'
+    df = pd.read_csv(path, sep='\t')
+    expected = 3
+    # Act
+    result = departure_cities(df)
+    # Assert
+    assert result[0] == expected
+
+
+
+def test_relation_manufacter():
+    # Arrange
+    path = 'C:/Users//Rev07//PycharmProjects//data2//test//file//flight.csv'
+    path2 = 'C:/Users//Rev07//PycharmProjects//data2//test//file//planes.csv'
+    df = pd.read_csv(path, sep='\t')
+    df2 = pd.read_csv(path2, sep='\t')
+    expected = 105.0
+    # Act
+    result = relation_manufacter(df,df2)
+    # Assert
+    assert result[0] == expected
+
+
+
+print(test_relation_manufacter())
+
+
